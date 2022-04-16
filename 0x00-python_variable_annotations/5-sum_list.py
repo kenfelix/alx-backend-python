2237d96561af0e8eb46486eac8e39f191b852244
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-04-16 22:12:12 +0100

    Update 5-sum_list.py

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-"""This module contains functions to perfom sum of lists"""
+"""This module contains modules to perform sum of lists"""
 from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
-    """takes sum of list"""
-    return sum(Input_list)
+    """return sum of list"""
+    return sum(input_list)
