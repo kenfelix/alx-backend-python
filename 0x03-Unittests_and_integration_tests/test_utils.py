826eec60e96e8f07bc9b3de418b957cce218817d
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-05-07 21:11:08 +0100

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -17,4 +17,15 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, map, path, expected_output):
         """returns the expected output"""
         self.assertEqual(access_nested_map(map, path), expected_output)
+        
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+    ])
+    def test_access_nested_map_exception(self, map, path, wrong_output):
+        """returns the expected output"""
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(map, path)
+
+        self.assertEqual(str(e.exception)[1:-1], wrong_output)
 
