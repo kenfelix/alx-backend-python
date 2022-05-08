6cf687a9de199b5a3f54f2843e77a6fe72173f9e
kenfelix <65518572+kenfelix@users.noreply.github.com>
2022-05-08 17:26:51 +0100

    Create test_client.py

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Contains test modules for the client package
+"""
+import unittest
+from unittest.mock import patch
+from parameterized import parameterized
+
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """TestGithubOrgClient class"""
+    
+    def
